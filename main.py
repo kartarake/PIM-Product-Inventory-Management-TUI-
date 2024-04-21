21a5b384ccf8c93c256b912288684c141bee48cd
kartarake <kar.prahveen@gmail.com>
2024-04-21 11:41:29 +0530

    Landerpage completed.

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from modules.database import kardb
+from modules.boxify import boxify
 
 import modules.accounts
 
 import credentials
+
+
+def landerpage():
+    str1 = """__        _______ _     ____ ___  __  __ _____   _____ ___    ____ ___ __  __ 
+    \ \      / / ____| |   / ___/ _ \|  \/  | ____| |_   _/ _ \  |  _ \_ _|  \/  |
+     \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|     | || | | | | |_) | || |\/| |
+      \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| | |  __/| || |  | |
+       \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/  |_|  |___|_|  |_|
+    """
+    print(boxify(str1,width = 170, align="centre"))
+    str2 = "[1] Login      |      [2] Sign up"
+    print(boxify(str2,width = 170, align="centre"))
+
+    while True:
+        choice = input('Enter respective number to continue : ')
+
+        if choice in ('1', '2','login','signup'):
+            break
+        else:
+            print(boxify('Invalid choice', width = 170, align = "centre"))
+
+    if choice in ('1', 'login'):
+        return 'login'
+    else:
+        return 'signup'
+
+def createtable(db, name):
+    try:
+        db.createdoc(name)
+    except NameError:
+        pass
 
 def main():
     # connecting to the mysql database
@@ -14,15 +46,12 @@ def main():
     )
 
     # creating tables
-    print(db.getdoclist())
-    try:
-        db.createdoc("credentials")
-    except NameError:
-        pass
+    createtable(db, "credentials")
 
     # app
-    modules.accounts.new_account(db, "kar", "abcd")
+    followup = landerpage()
     
+    #final steps
     db.disconnect()
 
 if __name__ == "__main__":
