ea0a72cebfa2e6efebfff24bc84a4cd77a8b0f72
kartarake <kar.prahveen@gmail.com>
2024-07-13 10:36:30 +0530

    Revamping accounts.py to new schema

--- modules/accounts.py
+++ modules/accounts.py
@@ -1,5 +1,6 @@
 import hashlib
 import secrets
+import database
 
 def gensalt(n):
     charlist = list(range(48,58)) + list(range(65,91)) + list(range(97, 123))
@@ -25,48 +26,47 @@ def verifyhash(stored_password, provided_password, salt):
     else:
         return False
     
-def new_account(db, username, password):
+def getuserlist(con):
+    # returns a list of all usernames.
+    data = database.fetchtable(con, "credentials")
+    usernamelist = [row[0] for row in data]
+    return usernamelist
+    
+def new_account(con, username, password):
     # use this to create new account in db.
     try:
-        db.changedoc('credentials')
-        data = db.data
+        data = database.fetchtable(con, "credentials")
+        usernamelist = getuserlist(con)
 
-        if username in data:
+        if username in usernamelist:
             raise ValueError('Username already exists.')
         else:
             salt = gensalt(16)
             hashed_password = hashmash(password, salt)
-
-            usercreds = {
-                "password":hashed_password,
-                "salt":salt
-            }
-
-            db.updatedoc(branch = {username:usercreds})
+            
+            record = (username, hashed_password, salt)
+            database.insertrow(con, "credentials", record)
 
         return 1
     
     except ValueError:
-        return 0
-    
-def getuserlist(db):
-    # returns a list of all usernames.
-    db.changedoc("credentials")
-    userlist = list(db.data.keys())
-    return userlist
+        return 0  
 
-def doespassmatch(db, provided_username, provided_password):
+def doespassmatch(con, provided_username, provided_password):
     # checks if the provided username matches with stored one.
-    db.changedoc("credentials")
-    data = db.data
+    data = database.fetchtable(con, "credentials")
 
-    if not(provided_username in getuserlist(db)):
+    if not(provided_username in getuserlist(con)):
         raise ValueError("Username not in data")
     else:
         pass
+    
+    for row in data:
+        if row[0] == provided_username:
+            break  
 
-    password = data[provided_username]["password"]
-    salt = data[provided_username]["salt"]
+    password = row[1]
+    salt = row[2]
 
     # returns true if password matches or returns false.
     return verifyhash(password, provided_password, salt)
