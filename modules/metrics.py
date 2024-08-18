77ed4f1c21f25622d409f29c56b34b6a8465755c
kartarake <kar.prahveen@gmail.com>
2024-08-18 14:19:02 +0530

    Unwanted timeperiod func removed.

--- modules/metrics.py
+++ modules/metrics.py
@@ -1,19 +1,5 @@
 import shops
 
-def totaltimeperiod(con, lwshop):
-    data = shops.fetchchanges(con, lwshop)
-    firstrow = data[0]
-    lastrow = data[-1]
-
-    start = str(firstrow[2])
-    end = str(lastrow[2])
-
-    start = int(start[0:4])*365 + int(start[5:7])*30 + int(start[8:10])
-    end = int(end[0:4])*365 + int(end[5:7])*30 + int(end[8:10])
-
-    timeperiod = end - start
-    return timeperiod
-
 def days(timestamp):
     days = int(timestamp[0:4])*365 + int(timestamp[5:7])*30 + int(timestamp[8:10])
     return days
